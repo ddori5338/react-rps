e802717cb553a428bac57893d19ff0cf21035362
youngchangyoo <ddori5338@hanmail.net>
2024-03-20 20:46:40 +0900

    chapter 6

--- src/App.js
+++ src/App.js
@@ -20,24 +20,44 @@ const choice = {
   },
   paper: {
     name: "Paper",
-    img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg"
+    img: "https://cdn11.bigcommerce.com/s-2i5mq6440u/images/stencil/2048x2048/products/3762/9095/PlasticPaper-CutSheet__18809.1597757191.png?c=2"
   }
 };
 
 function App() {
 
   const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
 
   const play = (userChoice) => {
     console.log('선택됨', userChoice);
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(choice[computerChoice]);
+    setResult(judgement(choice[userChoice], choice[computerChoice]));
+  }
+
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) return "tie";
+    if (user.name === "Rock") return computer.name === "Scissors" ? "win" : "lose";
+    if (user.name === "Paper") return computer.name === "Rock" ? "win" : "lose";
+    if (user.name === "Scissors") return computer.name === "Paper" ? "win" : "lose";
+  }
+
+  const randomChoice = () => {
+    // Object.keys: 매개변수로 주어진 객체의 키값만 뽑아서 배열로 반환하는 함수
+    let itemArray = Object.keys(choice);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return final;
   }
 
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect}/>
-        <Box title="Computer" item={userSelect}/>
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result === "tie" ? "tie" : result === "win" ? "lose" : result === "lose" ? "win" : ""}/>
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Box = (props) => {
     console.log("props:", props);
     return (
-        <div className="box">
+        <div className="box" id={props.result}>
             <h1>{props.title}</h1>
             <img className="item-img" src={props.item && props.item.img}/>
-            <h2>Win</h2>
+            <h2>{props.result}</h2>
         </div>
     )
 }
